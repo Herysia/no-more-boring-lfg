7ff331fc1207cc59a15a41dcf65cd848cbb4877a
Thomas GIRERD <totogirerd@gmail.com>
2019-12-26 22:34:53 +0100

    Fix when having multiple lfg pages

--- index.js
+++ index.js
@@ -4,42 +4,18 @@ module.exports = function NoMoreBoringLFG(mod) {
     var enabled = true;
     var lfgs;
     var lfgPerPage;
+	const listingsPerPage = 16;
     mod.hook('S_SHOW_PARTY_MATCH_INFO', 1, (e) => {
         if (!enabled) return;
-        if (e.pageCount == 0) //only one page
-        {
-            for (var i = 0; i < e.listings.length; i++) {
-                var str = e.listings[i].message.toLowerCase();
-                if (str.includes('wts') || str.includes('wtb')) {
-                    e.listings.splice(i, 1);
-                    i--;
-                }
-            }
-            return true;
-        } else {
-            if (e.pageCurrent == 0) {
-                lfgs = [];
-                lfgPerPage = e.listings.length;
-            }
-            for (var i = 0; i < e.listings.length; i++) {
-                var str = e.listings[i].message.toLowerCase();
-                if (!(str.includes('wts') || str.includes('wtb'))) {
-                    lfgs.push(e.listings[i]);
-                }
-            }
-            if (e.pageCurrent == e.pageCount) {
-                var pageCount = parseInt(lfgs.length / lfgPerPage);
-                for (var i = 0; i <= pageCount; i++) {
-                    mod.send('S_SHOW_PARTY_MATCH_INFO', 1, {
-                        pageCurrent: i,
-                        pageCount: pageCount,
-                        listings: lfgs.slice(lfgPerPage * i, lfgPerPage * (i + 1))
-                    })
-                }
-
-            }
-            return false;
-        }
+		for (var i = 0; i < e.listings.length; i++) {
+			var str = e.listings[i].message.toLowerCase();
+			if (str.includes('wts') || str.includes('wtb')) {
+				e.listings.splice(i, 1);
+				i--;
+			}
+		}
+		e.pageCount = parseInt(e.listings % listingsPerPage);
+		return true;
     })
     mod.command.add('lfg', () => {
         enabled = !enabled;
